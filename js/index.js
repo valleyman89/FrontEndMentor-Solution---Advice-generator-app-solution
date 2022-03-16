d80ac8900ef9ac6afc5882b23c90127a8dbd2ea5
valleyman89 <dev@stevenrolph.com>
2022-03-16 20:26:36 +0000

    comment the javascript

--- js/index.js
+++ js/index.js
@@ -1,13 +1,18 @@
 async function grabAdvice() {
+  // set URL and get the JSON result
   const adviceURL = "https://api.adviceslip.com/advice";
   const req = new Request(adviceURL);
   const res = await fetch(req);
   const adviceDetails = await res.text();
-  var adviceObject = JSON.parse(adviceDetails);
 
+  // turn it into an object
+  let adviceObject = JSON.parse(adviceDetails);
+
+  // manipulate the DOM and populate with the object result
   document.getElementById("adviceId").innerText = adviceObject["slip"]["id"];
   document.getElementsByTagName("p")[0].innerText =
     adviceObject["slip"]["advice"];
 }
 
+// call the function
 grabAdvice();
